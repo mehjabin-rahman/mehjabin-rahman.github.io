7012f989b3ebb26bad7ba20756409483902953df
mehjabin-rahman <mehjabin.rahman.ame@gmail.com>
2025-04-26 18:27:07 -0400

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,7 +1,6 @@
 const about = {
     name: "Mehjabin Rahman",
     description: "Data Science and AI professional with experience in building, analyzing, and validating machine learning models,including deep learning and NLP solutions. Skilled in Python, SQL, and cloud platforms (GCP, AWS), with expertise in data analysis, model explainability, and dashboard reporting (Power BI, Tableau). Strong focus on ensuring model robustness, fairness, and risk governance to support safe and responsible AI deployment. Let's connect!",
-    image: "#",
     cv: "#",
     email: "mailto:mehjabin.rahman.ame@gmail.com",
     linkedin: "https://www.linkedin.com/in/mehjabin-rahman/",
